92c96d08c2706e8be9b39ab1a839919f1fcad5eb
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-13 19:51:20 -0300

    alt disponibilização de categorias

--- src/Components/Icons/index.js
+++ src/Components/Icons/index.js
@@ -4,51 +4,35 @@ export default function Icons() {
     return (
         <div className="d-flex justify-content-center align-items-center div-central">
 
-            <div className="categoria-icon d-flex justify-content-center align-items-center">
-                <a className="navbar-brand d-flex flex-column justify-content-center align-items-center" href="/">
+            <div className="div-icon d-flex flex-column justify-content-center align-items-center">
+                <a className="" href="/">
                     <img src="./assets/img/logo.svg" alt="Logo Sarton" width="100" height="40" />
-                    <span>Eletronicos</span>
                 </a>
+                <span>máquinas</span>
             </div>
 
-            <div className="categoria-icon d-flex justify-content-center align-items-center">
-                <a className="navbar-brand d-flex flex-column justify-content-center align-items-center" href="/">
+            <div className="div-icon d-flex flex-column justify-content-center align-items-center">
+                <a className="" href="/">
                     <img src="./assets/img/logo.svg" alt="Logo Sarton" width="100" height="40" />
-                    <span>Escritório</span>
                 </a>
-
+                <span>imóveis</span>
             </div>
 
-            <div className="categoria-icon d-flex justify-content-center align-items-center">
-
-                <a className="navbar-brand d-flex flex-column justify-content-center align-items-center" href="/">
+            <div className="div-icon d-flex flex-column justify-content-center align-items-center">
+                <a className="" href="/">
                     <img src="./assets/img/logo.svg" alt="Logo Sarton" width="100" height="40" />
-                    <span>Sl Arquivos</span>
-
                 </a>
-            </div>
-            <div className="categoria-icon d-flex justify-content-center align-items-center">
-
-                <a className="navbar-brand d-flex flex-column justify-content-center align-items-center" href="/">
-                    <img src="./assets/img/logo.svg" alt="Logo Sarton" width="100" height="40" />
-                    <span>Banheiro</span>
-                    
-                </a>
-                
-
-            </div>
-            <div className ="categoria-icon d-flex justify-content-center align-items-center">
-                <a className="navbar-brand d-flex flex-column justify-content-center align-items-center" href="/">
-                    <img src="./assets/img/logo.svg" alt="Logo Sarton" width="100" height="40" />
-                    <span>Automóveis</span>
-
-                </a>
-
+                <span>computadores</span>
             </div>
 
+            <div className="div-icon d-flex flex-column justify-content-center align-items-center">
+                <a className="" href="/">
+                    <img src="./assets/img/logo.svg" alt="Logo Sarton" width="100" height="40" />
+                </a>
+                <span>veículos</span>
+            </div>
 
-            <div className="margin"></div>
+            
         </div>
-
     )
 }
