485ea63a2913225f7660e6b75aa65388e3aeb60a
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-13 22:10:30 -0300

    add border

--- src/Components/Icons/index.js
+++ src/Components/Icons/index.js
@@ -2,30 +2,36 @@ import style from "./Icons.css"
 
 export default function Icons() {
     return (
-        <div className="d-flex justify-content-center align-items-center div-central">
 
-            <div className="div-icon d-flex flex-column justify-content-center align-items-center">
-                <a className="" href="/">
-                    <img src="./assets/img/Icons/note.svg" alt="Logo Sarton" width="100" height="40" />
-                </a>
-                <span>Eletrônicos</span>
+        <div className="d-flex justify-content-center align-items-center flex-column">
+
+            <div className="d-flex justify-content-center align-items-center div-central">
+
+                <div className="div-icon d-flex flex-column justify-content-center align-items-center">
+                    <a className="" href="/eletronicos">
+                        <img src="./assets/img/Icons/note.svg" alt="Logo Sarton" width="100" height="40" />
+                    </a>
+                    <span>Eletrônicos</span>
+                </div>
+
+                <div className="div-icon d-flex flex-column justify-content-center align-items-center">
+                    <a className="" href="/">
+                        <img src="./assets/img/Icons/cadeira.svg" alt="Logo Sarton" width="100" height="40" />
+                    </a>
+                    <span>Mobília</span>
+                </div>
+
+                <div className="div-icon d-flex flex-column justify-content-center align-items-center">
+                    <a className="" href="/">
+                        <img src="./assets/img/Icons/carro.svg" alt="Logo Sarton" width="100" height="40" />
+                    </a>
+                    <span>veículos</span>
+                </div>
+
             </div>
 
-            <div className="div-icon d-flex flex-column justify-content-center align-items-center">
-                <a className="" href="/">
-                    <img src="./assets/img/Icons/cadeira.svg" alt="Logo Sarton" width="100" height="40" />
-                </a>
-                <span>Mobília</span>
-            </div>
+            <div className="margin"></div>
 
-            <div className="div-icon d-flex flex-column justify-content-center align-items-center">
-                <a className="" href="/">
-                    <img src="./assets/img/Icons/carro.svg" alt="Logo Sarton" width="100" height="40" />
-                </a>
-                <span>veículos</span>
-            </div>
-
-            
         </div>
     )
 }
