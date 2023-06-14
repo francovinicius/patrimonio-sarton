b4956c0cc1277a4f814a426b61b7a71b12363444
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-14 16:29:17 -0300

    adicionando modal

--- src/Components/Eletronics/index.js
+++ src/Components/Eletronics/index.js
@@ -2,19 +2,43 @@ import styles from "./Eletronics.css"
 export default function Eletronics() {
     return (
 
-        
 
-            <div className="div-eletronics d-flex justify-content-center align-items-center">
 
-                <div class="card d-flex justify-content-center align-items-center flex-column">
-                    <img src="./assets/img/Icons/note.svg" class="card-img-top" alt="..." />
-                    <div class="card-body d-flex justify-content-center align-items-center flex-column">
-                        <h5 class="card-title">Patrimônio #333</h5>
-                        <a href="#" class="btn btn-color-blue">Ver Status</a>
-                    </div>
+        <div className="div-eletronics d-flex justify-content-center align-items-center">
+
+            <div class="card d-flex justify-content-center align-items-center flex-column">
+                <img src="./assets/img/Icons/note.svg" class="card-img-top" alt="..." />
+                <div class="card-body d-flex justify-content-center align-items-center flex-column">
+                    <h5 class="card-title">Patrimônio #333</h5>
+
+                    {/* <!-- Button trigger modal -->*/}
+                    <button type="button" class="btn btn-color-blue" data-bs-toggle="modal" data-bs-target="#pat333">
+                        Ver Status
+                    </button>
+
                 </div>
 
+
+
+                {/*<!-- Modal -->*/}
+                <div class="modal fade" id="pat333" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+        </div>
 
     )
 }
